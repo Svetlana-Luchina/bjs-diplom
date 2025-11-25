30a4414e445aed0bf0c1383c4d3ed30aca908dd7
Svetlana Luchina <luchsveta3490@gmail.com>
2025-11-25 22:43:16 +0300

    Выполнены пополнение, конвертация

--- public/homePage.js
+++ public/homePage.js
@@ -24,7 +24,45 @@ function exchangaRate() {
          ratesBoard.fillTable(response.data);
         }
     });
-}
+};
 
 exchangaRate();
 setInterval(exchangaRate, 6000);
+
+// Операции с деньгами
+// Пополнение баланса
+const moneyManager = new MoneyManager();
+moneyManager.addMoneyCallback = (data) => {
+  ApiConnector.addMoney(data, (response) => {
+    if (response.success) {
+      ProfileWidget.showProfile(response.data);
+      moneyManager.setMessage(true, 'Баланс успешно пополнен');
+    } else {
+      moneyManager.setMessage(false, response.error);
+    }
+  });
+};
+
+// Конвертирование валюты
+moneyManager.conversionMoneyCallback = (data) => {
+  ApiConnector.convertMoney(data, (response) => {
+    if (response.success) {
+      ProfileWidget.showProfile(response.data);
+      moneyManager.setMessage(true, 'Конвертация выполнена успешно');
+    } else {
+      moneyManager.setMessage(false, response.error);
+    }
+  });
+};
+
+// Перевод средств
+moneyManager.sendMoneyCallback = (data) => {
+  ApiConnector.transferMoney(data, (response) => {
+    if (response.success) {
+      ProfileWidget.showProfile(response.data);
+      moneyManager.setMessage(true, 'Перевод выполнен успешно');
+    } else {
+      moneyManager.setMessage(false, response.error);
+    }
+  });
+};
